d8b984c5af3854195af73c588d98a3a1950d0e95
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-02-09 04:43:46 -0600

    added a submit button and input field to add more Todo Items

--- todo_app/src/components/Todos.vue
+++ todo_app/src/components/Todos.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
-
     <div v-bind:key="todo.id" v-for="todo in todos">
-        <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
     </div>
-
+    <AddTodo />
   </div>
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue';
+import AddTodo from "./AddTodo.vue";
+import TodoItem from "./TodoItem.vue";
 
 export default {
   name: "Todos",
   components: {
-      TodoItem
+    TodoItem,
+    AddTodo
   },
   props: ["todos"]
 };
