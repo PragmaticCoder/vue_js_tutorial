2e309a032819b5cfb706c9f4b0b0b3e856984f78
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-02-11 07:01:45 -0600

    Added an increment counter

--- udemy_crash_course/src/App.vue
+++ udemy_crash_course/src/App.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <TutorialComponent name="Delilah" imgSrc="http://cloudcareersolutions.com/wp-content/uploads/2018/09/module-6.jpg"/>
     <TutorialComponent name="Alvi"/>
+
   </div>
 </template>
 
--- udemy_crash_course/src/components/TutorialComponent.vue
+++ udemy_crash_course/src/components/TutorialComponent.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="my-app">
-    <h1>Hey There {{ name.split('').reverse().join('')}}! How's it like in New York City!?</h1>
-    <img :src="imgSrc">
+    <h1>
+      Hey There
+      {{
+        name
+          .split("")
+          .reverse()
+          .join("")
+      }}! How's it like in New York City!?
+    </h1>
+
+    <h1>Counter Example</h1>
+    <h2>Counter: {{ counter }}</h2>
+
+    <button @click="increase">Increase</button>
+    <br />
+
+    <img :src="imgSrc" />
+    <hr />
   </div>
 </template>
 
 <script>
 export default {
   name: "TutorialComponent",
-  props: ["name", "imgSrc"]
+  props: ["name", "imgSrc"],
+  data: function() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    increase: function() {
+      this.counter += 1;
+    }
+  }
 };
 </script>
 
