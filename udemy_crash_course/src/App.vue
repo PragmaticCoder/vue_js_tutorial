297fa11ec9ac45bd6df0daa1ef0675b5a295ff90
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-02-10 23:30:16 -0600

    Created a new component from scratch - AppComponent

--- udemy_crash_course/src/App.vue
+++ udemy_crash_course/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TutorialComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TutorialComponent from "./components/TutorialComponent.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TutorialComponent
   }
 }
 </script>
---
+++ udemy_crash_course/src/components/TutorialComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello Component
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TutorialComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
